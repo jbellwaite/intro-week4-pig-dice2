6b98b149c38c94a71698a84f3f48c638db3862d0
Jessie Waite and Rakhee Gandhi <jessie@>
2017-09-28 11:01:42 -0700

    Add game functionality and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ function Player (player) {
   this.playerName = player;
   this.subTotal = [];
   this.grand = 0;
-  this.whoTurn = 0;
-
 }
 
 Player.prototype.getRandomNumber=function(min, max) {
@@ -13,30 +11,29 @@ Player.prototype.getRandomNumber=function(min, max) {
 }
 
 Player.prototype.grandTotal = function() {
+  var grandTotal = this.grand;
   if (this.subTotal.length > 0) {
   var sum = this.subTotal.reduce((a, b)=> a+b);
-  return sum + this.grand;
-}else {
-  return this.grand;
-}
-}
-
-  Player.prototype.zero = function () {
-    while (this.subTotal.length) {
-      this.subTotal.pop();
-    }
+  this.grand = (sum + grandTotal);
   }
+}
 
-  // var arrayInt = parseInt(array);
-  // for (i = 0, sum = 0; i <= array.length; sum += array[i++]){ write logic }
-  // for (i = 0; i <= array.length; i++);
-  // return this.grand +=array[i];
+Player.prototype.zero = function () {
+    this.subTotal=[];
+}
+
+Player.prototype.win = function() {
+  if (this.grand >= 20) {
+    $("#win").show();
+  }
+}
 
 
 
 $(document).ready(function(){
 
   var newPlayer1 = new Player("Player 1");
+  var newPlayer2 = new Player("Player 2");
 
   $("#roll1").click(function(event) {
     event.preventDefault();
@@ -46,23 +43,64 @@ $(document).ready(function(){
     var player1Roll = newPlayer1.getRandomNumber();
 
     if (player1Roll === 1) {
-      alert("You rolled a 1, you lose all points this round");
+      alert("Player 1, you rolled a 1, you lose all points this round and it's Player 2's turn.");
       $("ul#bank1").text("");
       newPlayer1.zero();
+      newPlayer1.grandTotal();
+      $("#roll1, #hold1, #p1turn").hide();
+      $("#roll2, #hold2, #p2turn").show();
     } else {
       $("#bank1").prepend("<li>" + player1Roll + "</li>");
       var player1RollInt = parseInt(player1Roll);
       newPlayer1.subTotal.push(player1RollInt);
       console.log(newPlayer1.subTotal);
     }
-    // newPlayer1.grandTotal();
-    // newPlayer2.grandTotal();
   });
 
   $("#hold1").click(function(event) {
     event.preventDefault();
-    var player1gT = newPlayer1.grandTotal();
-    $("#total1").text(player1gT);
+    newPlayer1.grandTotal();
+    $("#total1").text(newPlayer1.grand);
     $("#bank1").text("");
+    newPlayer1.zero();
+    $("#roll1, #hold1, #p1turn").hide();
+    $("#roll2, #hold2, #p2turn").show();
+    newPlayer1.win();
   });
+
+//Code for Player 2
+  $("#roll2").click(function(event) {
+    event.preventDefault();
+
+    console.log(newPlayer2.playerName);
+
+    var player2Roll = newPlayer2.getRandomNumber();
+
+    if (player2Roll === 1) {
+      alert("Player 2, you rolled a 1, you lose all points this round and it's Player 1's turn.");
+      $("ul#bank2").text("");
+      newPlayer2.zero();
+      newPlayer2.grandTotal();
+      $("#roll1, #hold1, #p1turn").show();
+      $("#roll2, #hold2, #p2turn").hide();
+
+    } else {
+      $("#bank2").prepend("<li>" + player2Roll + "</li>");
+      var player2RollInt = parseInt(player2Roll);
+      newPlayer2.subTotal.push(player2RollInt);
+      console.log(newPlayer2.subTotal);
+    }
+  });
+
+  $("#hold2").click(function(event) {
+    event.preventDefault();
+    newPlayer2.grandTotal();
+    $("#total2").text(newPlayer2.grand);
+    $("#bank2").text("");
+    newPlayer2.zero();
+    $("#roll1, #hold1, #p1turn").show();
+    $("#roll2, #hold2, #p2turn").hide();
+    newPlayer2.win();
+  });
+
 });
